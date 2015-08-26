162c0b9cebccc942fed7497e3402ff60d468041b
gordfoo <gordfoo@gmail.com>
2015-08-26 08:28:17 -0600

    Create source.py

---
+++ source.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+raw_word_table = {}
+
+class wellobj
+    def __init__(self,name):
+        self.name = name
+    
+    def addshow(formation, depth):
+        
+    
+
+#Parse the block of text into individual words
+def parseword(string)
+
+def splitword(textblock):
+    
+    
+
+while TRUE
